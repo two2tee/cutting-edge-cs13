0440732b14114cd19c88ba0bceaa0a2dd0de26a8
Dennis Thinh Tan Nguyen <dttn@di.dk>
2025-05-20 15:26:30 +0200

    style: format test section with region directives for better organization

--- Modules/06/Labs/Lab 06.1/Ongoing/Collection Expressions/Program.cs
+++ Modules/06/Labs/Lab 06.1/Ongoing/Collection Expressions/Program.cs
@@ -13,7 +13,7 @@ static List<int> Merge(List<int> list1, List<int> list2, List<int> acc) =>
 
 
 
-// Test:
+#region  Test:
 
 List<int> elements1 = [11, 22, 44, 77, 99];
 List<int> elements2 = [33, 44, 55, 66, 88, 99];
@@ -33,3 +33,5 @@ Console.WriteLine(result.SequenceEqual(expected)
     ? "\n\nTest passed!"
     : "\n\nTest failed!"
 );
+
+#endregion
